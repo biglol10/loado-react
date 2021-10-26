a4b53edde828ed62f6dff942d0154e04684bab23
변지욱 사원 DCX플랫폼팀 (jiwookbyun@lgcns.com <biglol@nate.com>
2021-10-26 17:25:02 +0900

    dashboard changed

--- src/Components/Utils/ProfileModal.js
+++ src/Components/Utils/ProfileModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import {
   Modal,
   Header,
@@ -7,32 +7,33 @@ import {
   Icon,
   Divider,
   Image,
-} from 'semantic-ui-react';
-import Cropper from 'react-cropper';
-import uploadPic from '../Utils/uploadPicToCloudinary';
-import { updateProfilePic, waitForSomeTime } from '../Utils/ViewDataUtil';
-import cookie from 'js-cookie';
+} from "semantic-ui-react";
+import Cropper from "react-cropper";
+import uploadPic from "../Utils/uploadPicToCloudinary";
+import { updateProfilePic, waitForSomeTime } from "../Utils/ViewDataUtil";
+import cookie from "js-cookie";
 
 function ProfileModal({ profileModal, setProfileModal, profilePic }) {
   const [cropper, setCropper] = useState();
   const [media, setMedia] = useState(null);
   const [mediaPreview, setMediaPreview] = useState();
   const [statusText, setStatusText] = useState(
-    '이미지를 업로드하고 잘라보세요 (icon)'
+    "이미지를 업로드하고 잘라보세요 (icon)"
   );
   const inputRef = useRef();
 
   const handleImageChange = (e) => {
     const { name, value, files } = e.target;
-    if (name === 'media') {
+    if (name === "media") {
       setMedia(files[0]);
       setMediaPreview(URL.createObjectURL(files[0]));
+      // console.log("objectURL is ", URL.createObjectURL(files[0]));  https://kyounghwan01.github.io/blog/JS/JSbasic/Blob-url/#createobjecturl
     }
   };
 
   const cropperStyle = {
-    height: '400px',
-    width: '100%',
+    height: "400px",
+    width: "100%",
   };
 
   const submitData = async () => {
@@ -40,27 +41,27 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
     let uploadResult = false;
 
     if (false) {
-      alert('ASDF');
+      alert("ASDF");
       setMedia(cropper.getCroppedCanvas().toDataURL()); // passing actual crop image
       //   setMediaPreview(cropper.getCroppedCanvas().toDataURL())
       cropper.destroy(); // release the cropper from the memory
     }
 
     if (media !== null && cropper) {
-      setStatusText('이미지를 업로드 중입니다');
+      setStatusText("이미지를 업로드 중입니다");
       picUrl = await uploadPic(cropper.getCroppedCanvas().toDataURL());
       waitForSomeTime(500);
       cropper.destroy();
       if (!picUrl) {
-        setStatusText('이미지 업로드에 실패했습니다');
+        setStatusText("이미지 업로드에 실패했습니다");
       }
       uploadResult = await updateProfilePic(
         picUrl,
-        cookie.get('loadoUserToken')
+        cookie.get("loadoUserToken")
       );
     }
     if (!uploadResult) {
-      setStatusText('이미지 업로드에 실패했습니다');
+      setStatusText("이미지 업로드에 실패했습니다");
       return;
     }
     setProfileModal(false);
@@ -71,24 +72,24 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
     <>
       <Modal
         closeOnDimmerClick={false}
-        size='large'
+        size="large"
         // onClose={setProfileModal(false)}
         open={profileModal}
       >
         <Modal.Header>
-          {statusText}{' '}
+          {statusText}{" "}
           <Icon
-            name='image'
-            style={{ color: 'red', cursor: 'pointer' }}
+            name="image"
+            style={{ color: "red", cursor: "pointer" }}
             onClick={() => inputRef.current.click()}
           />
           <input
             ref={inputRef}
             onChange={handleImageChange}
-            name='media'
-            style={{ display: 'none' }}
-            type='file'
-            accept='image/*'
+            name="media"
+            style={{ display: "none" }}
+            type="file"
+            accept="image/*"
           />
         </Modal.Header>
         <Grid columns={2}>
@@ -102,9 +103,9 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
                   highlight
                   responsive
                   guides
-                  dragMode='move'
+                  dragMode="move"
                   initialAspectRatio={1}
-                  preview='.img-preview'
+                  preview=".img-preview"
                   src={mediaPreview}
                   viewMode={1}
                   minCropBoxHeight={10}
@@ -116,8 +117,8 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
                 />
               ) : (
                 <Image
-                  src='https://react.semantic-ui.com/images/wireframe/image.png'
-                  style={{ ...cropperStyle, cursor: 'pointer' }}
+                  src="https://react.semantic-ui.com/images/wireframe/image.png"
+                  style={{ ...cropperStyle, cursor: "pointer" }}
                   onClick={() => inputRef.current.click()}
                 />
               )}
@@ -126,23 +127,23 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
           </Grid.Column>
           <Grid.Column>
             <Modal.Content image>
-              <div style={{ margin: '15px 0px' }}>
-                <Header as='h3'>
-                  <Icon name='file image outline' />
-                  <Header.Content content='최종 이미지' />
+              <div style={{ margin: "15px 0px" }}>
+                <Header as="h3">
+                  <Icon name="file image outline" />
+                  <Header.Content content="최종 이미지" />
                 </Header>
               </div>
               <div>
                 <div
                   style={{
-                    width: '100%',
-                    height: '300px',
-                    display: 'inline-block',
-                    padding: '10px',
-                    overflow: 'hidden',
-                    boxSizing: 'border-box',
+                    width: "100%",
+                    height: "300px",
+                    display: "inline-block",
+                    padding: "10px",
+                    overflow: "hidden",
+                    boxSizing: "border-box",
                   }}
-                  className='img-preview'
+                  className="img-preview"
                 ></div>
               </div>
             </Modal.Content>
@@ -150,8 +151,8 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
         </Grid>
         <Modal.Actions>
           <Button
-            title='Reset (R)'
-            icon='redo'
+            title="Reset (R)"
+            icon="redo"
             circular
             onClick={() => cropper && cropper.reset()}
           />
@@ -164,22 +165,22 @@ function ProfileModal({ profileModal, setProfileModal, profilePic }) {
           /> */}
 
           <Button
-            title='New Cropbox (C)'
-            icon='crop'
+            title="New Cropbox (C)"
+            icon="crop"
             circular
-            onClick={() => cropper && cropper.setDragMode('crop')}
+            onClick={() => cropper && cropper.setDragMode("crop")}
           />
 
           <Button
             negative
-            content='취소'
-            icon='cancel'
+            content="취소"
+            icon="cancel"
             onClick={() => setProfileModal(false)}
           />
 
           <Button
-            content='프로필 업로드'
-            icon='checkmark'
+            content="프로필 업로드"
+            icon="checkmark"
             positive
             onClick={submitData}
           />
